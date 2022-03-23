82563e49d0403a3c9531e480b75b753968dcb014
Ka :) <62915661+KCWB@users.noreply.github.com>
2022-03-22 21:20:14 -0300

    add json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { routes } from './config/router';
 
+
 const app = express();
 app.use(routes);
+app.use(express.json());
 
 app.listen(3333, () => {
     console.log('rodo deu certo. ')
